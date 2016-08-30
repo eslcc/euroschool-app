6db7f5cf4050c31cef3a12c3b37e098d9d1bdd34
Vidminas Mikucionis <HackerManiac@users.noreply.github.com>
2016-08-30 15:44:09 +0200

    Fixed some warnings

--- android/app/src/main/java/com/euroschool/MainApplication.java
+++ android/app/src/main/java/com/euroschool/MainApplication.java
@@ -1,14 +1,12 @@
 package com.euroschool;
 
 import android.app.Application;
-import android.util.Log;
 
 import com.facebook.react.ReactApplication;
-import com.github.xinthink.rnmk.ReactMaterialKitPackage;
-import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
+import com.github.xinthink.rnmk.ReactMaterialKitPackage;
 
 import java.util.Arrays;
 import java.util.List;
@@ -23,7 +21,7 @@ public class MainApplication extends Application implements ReactApplication {
 
     @Override
     protected List<ReactPackage> getPackages() {
-      return Arrays.<ReactPackage>asList(
+      return Arrays.asList(
           new MainReactPackage(),
             new ReactMaterialKitPackage()
       );
