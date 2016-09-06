beb96d1cd90857c7058e41843d62a2a7043b404a
Vidminas Mikucionis <HackerManiac@users.noreply.github.com>
2016-09-06 19:48:19 +0200

    Reduxification and navigation
    Enough for today
    
    Ran lintfix
    
    Lots of fixes and linter buttlicking
    
    Separated config and applied 1TBS
    
    Arrow functions
    
    New ESLint config
    
    Now the plugins are actually getting used, but I can't be arsed to read
    through all the rules, you do what you want with this.
    Updated npm packages and went back from git links, since we have that auto updater.
    Removed the deep-assign package, and it seems like nothing changed. No idea why it was there.
    
    App opens
    
    Copied over code from examples on the internet and navigation branch
    Hacked in transitions between pages (will need rework)
    
    Joined all reducers
    
    ES6 return blocks and arrow functions
    
    Trying to make scenes redux aware
    
    Started connect generation
    
    Apparently that's not how redux works
    
    Super slow transition
    
    Login no longer crashes
    
    removed eslint disables
    
    Fuck you @hackermaniac use equals signs properly.
    
    reduxify
    
    mini fixes
    
    this file should be ignored too
    
    fixed gitignore

--- common/lib/msm/schedule.js
+++ common/lib/msm/schedule.js
@@ -1,21 +1,18 @@
 // @flow
-
-import { doMsmRequest, METHODS } from './helpers';
 import moment from 'moment';
+import { doMsmRequest, METHODS } from './helpers';
 
 /**
- * Get the currently logged in user's schedule
- * @param  {moment} start The start of the period to return. Defaults to this Monday at 00:00:00.
- * @param {moment} end The end of the period to return. Defaults to start + 1 week
- * @return {Promise<array<object>>} The schedule entries.
- */
-export default function getSchedule(
-    start = (moment().isoWeekday(0).set({ hour: 0, minute: 0, second: 0 })),
-    end = (start.add(1, 'w').subtract(1, 'd'))) {
-  return doMsmRequest(METHODS.POST, '/data/common_handler.php?action=Contact::AJAX_U_GetSchedule', {
-    inc_appointment: true,
-    start: start.unix(),
-    end: end.unix(),
-  })
-  .then((response) => response.json());
-}
+* Get the currently logged in user's schedule
+* @param  {moment} start The start of the period to return. Defaults to this Monday at 00:00:00.
+* @param {moment} end The end of the period to return. Defaults to start + 1 week
+* @return {Promise<array<object>>} The schedule entries.
+*/
+export default (start = (moment().isoWeekday(0).set({ hour: 0, minute: 0, second: 0 })),
+                end = (start.add(1, 'w').subtract(1, 'd'))) =>
+    doMsmRequest(METHODS.POST, '/data/common_handler.php?action=Contact::AJAX_U_GetSchedule', {
+        inc_appointment: true,
+        start: start.unix(),
+        end: end.unix(),
+    })
+    .then((response) => response.json());
